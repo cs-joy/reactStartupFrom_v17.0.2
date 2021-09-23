b18c0e1b1c459f853120875b109ff133dd1a96a3
CsJoy0363 <latestworld9042@gmail.com>
2021-09-24 03:11:45 +0600

    commit v1.0.1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Welcome from './components/Welcome';
+import Comment from "./components/Comment";
+
+
 function App() {
+  const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,8 +28,16 @@ function App() {
           Learn React
         </a>
       </header>
+      <Welcome name="AJ" />
+      <Comment
+          date={comment.date}
+          text={comment.text}
+          author={comment.author}
+      />
+
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Welcome(props) {
+    return (
+        <div>
+            <h1>Hello, {props.name}</h1>
+        </div>
+    )
+}
+
+export default Welcome
