40b2c4cc399380bb4d74158b6ed4726860509fe5
Md Zahangir Alam(#Mr.) <latestworld9042@gmail.com>
2023-08-31 22:44:26 +0600

    removed!

--- glob.js
+++
@@ -1,9 +0,0 @@
-var glob = require("glob")
-
-// options is optional
-glob("**/*.js", options, function (er, files) {
-    // files is an array of filenames.
-    // If the `nonull` option is set, and nothing
-    // was found, then files is ["**/*.js"]
-    // er is an error object or null.
-})
